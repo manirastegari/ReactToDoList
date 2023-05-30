cb8f6f1d877eaf5b3d68266afaaa811bac962429
Mani Rastegari <mani.rastegari@gmail.com>
2023-05-30 10:26:11 -0400

    ChangesMadeEngine3FooterMongoose2dbjsv2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ToDoItem from "./ToDoItem";
 import InputArea from "./InputArea";
 import "./db";
@@ -7,6 +7,20 @@ import ToDo from "./ToDo";
 
 function App() {
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  function fetchItems() {
+    ToDo.find(function (err, foundItems) {
+      if (err) {
+        console.error(err);
+      } else {
+        setItems(foundItems.map(item => item.text));
+      }
+    });
+  }
 
   function addItem(inputText) {
     const newToDo = new ToDo({
@@ -17,16 +31,13 @@ function App() {
         console.error(err);
       } else {
         console.log("Item saved to the database");
-        setItems((prevItems) => [...prevItems, inputText]);
+        setItems(prevItems => [...prevItems, inputText]);
       }
     });
-    // setItems((prevItems) => {
-    //   return [...prevItems, inputText];
-    // });
   }
 
   function deleteItem(id) {
-    setItems((prevItems) => {
+    setItems(prevItems => {
       return prevItems.filter((item, index) => {
         return index !== id;
       });
@@ -56,3 +67,70 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import ToDoItem from "./ToDoItem";
+// import InputArea from "./InputArea";
+// import "./db";
+// import ToDo from "./ToDo";
+
+
+// function App() {
+//   const [items, setItems] = useState([]);
+
+//   function addItem(inputText) {
+//     const newToDo = new ToDo({
+//       text: inputText,
+//     });
+//     newToDo.save(function (err) {
+//       if (err) {
+//         console.error(err);
+//       } else {
+//         console.log("Item saved to the database");
+//         setItems((prevItems) => [...prevItems, inputText]);
+//       }
+//     });
+//     // setItems((prevItems) => {
+//     //   return [...prevItems, inputText];
+//     // });
+//   }
+
+//   function deleteItem(id) {
+//     setItems((prevItems) => {
+//       return prevItems.filter((item, index) => {
+//         return index !== id;
+//       });
+//     });
+//   }
+
+//   return (
+//     <div className="container">
+//       <div className="heading">
+//         <h1>To-Do List</h1>
+//       </div>
+//       <InputArea onAdd={addItem} />
+//       <div>
+//         <ul>
+//           {items.map((todoItem, index) => (
+//             <ToDoItem
+//               key={index}
+//               id={index}
+//               text={todoItem}
+//               onChecked={deleteItem}
+//             />
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
